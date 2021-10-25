450ab3720a7a73c5ec2da20f26d628dc10b9dcbb
Gécs Bernát <gecs.bernat18@gmail.com>
2021-10-25 12:18:43 +0200

    update

--- www/WatchConnect.js
+++ www/WatchConnect.js
@@ -1,26 +1,20 @@
 var exec = require('cordova/exec');
-
 module.exports = {
     updateApplicationContext: function (keyString, valueString, success, error) {
         exec(success, error, "WatchConnect", "updateApplicationContext", [keyString, valueString]);
     },
-
     initialize: function (success, error) {
         exec(success, error, "WatchConnect", "initialize", []);
     },
-
     deinitialize: function (success, error) {
         exec(success, error, "WatchConnect", "deinitialize", []);
     },
-
     sendMessage: function (message, success, error) {
         exec(success, error, "WatchConnect", "sendMessage", [message]);
     },
-
     listenMessage: function (onMessage) {
         exec(onMessage, null, "WatchConnect", "listenMessage", []);
     },
-
     checkConnection: function (success, error) {
         exec(success, error, "WatchConnect", "checkConnection", []);
     }
